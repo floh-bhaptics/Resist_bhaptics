b4ca90d133c741fb32f26221526682a93c1b0e54
floh-bhaptics <floh@fahrenbergers.de>
2022-12-10 21:45:21 +0100

    Added a bunch more interesting function for swinging mechanic.

--- Resist_bhaptics.cs
+++ Resist_bhaptics.cs
@@ -29,5 +29,72 @@ namespace Resist_bhaptics
                 tactsuitVr.StopThreads();
             }
         }
+
+        [HarmonyPatch(typeof(GrappleGun), "Swing", new Type[] { })]
+        public class bhaptics_Swing
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(GrappleGun), "CancelHook", new Type[] { })]
+        public class bhaptics_CancelHook
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(GrappleGun), "Cast", new Type[] { })]
+        public class bhaptics_CastHook
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(GrappleGun), "Hook", new Type[] { })]
+        public class bhaptics_Hook
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        [HarmonyPatch(typeof(GrappleGun), "Release", new Type[] { })]
+        public class bhaptics_HookRelease
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+        /*
+        [HarmonyPatch(typeof(GrappleGun), "UpdateVelocity", new Type[] { })]
+        public class bhaptics_UpdateVelocity
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrappleGun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
     }
 }
