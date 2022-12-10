0ecaa69b082db602391d4acfdf6f8b8d77985c3f
floh-bhaptics <floh@fahrenbergers.de>
2022-12-10 21:38:12 +0100

    Add project files.

---
+++ Resist_bhaptics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace Resist_bhaptics
+{
+    public class Resist_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(Gun), "Shoot", new Type[] { })]
+        public class bhaptics_GunShoot
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Gun __instance)
+            {
+                bool isRightHand = (!__instance.isLeft);
+                tactsuitVr.StopThreads();
+            }
+        }
+    }
+}
